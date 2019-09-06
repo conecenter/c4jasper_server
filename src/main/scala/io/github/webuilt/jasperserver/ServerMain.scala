acfdfab9b30fbe276db3220dafe8306e37540c8c
Aleksandr Senchenko <awb.senchenko@gmail.com>
2019-09-06 15:40:00 +0300

    driver fix

--- src/main/scala/io/github/webuilt/jasperserver/ServerMain.scala
+++ src/main/scala/io/github/webuilt/jasperserver/ServerMain.scala
@@ -3,6 +3,8 @@ package io.github.webuilt.jasperserver
 import java.io.{ByteArrayOutputStream, File, PrintWriter}
 import java.nio.file.{Files, Paths}
 import java.sql.DriverManager
+import java.util
+import java.util.Properties
 
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
@@ -19,10 +21,14 @@ import net.sf.jasperreports.export.{SimpleExporterInput, SimpleOutputStreamExpor
 
 import scala.concurrent.{ExecutionContextExecutor, Future}
 import scala.io.StdIn
-import scala.util.matching.Regex
 import scala.util.{Try, Using}
 
 object ServerMain extends App with ImplicitLazyLogging {
+  // regexes
+  val jrxmlR = """(.*)\.jrxml""".r
+  val pdfR = """(.*)\.pdf""".r
+  val dbUrlExtractR = """jdbc:my:url=(.*)&user=(.*)""".r
+  // akka
   info"Starting Jasper Server App"
   debug"Preparing Akka ecosystem"
   implicit val system: ActorSystem = ActorSystem("my-system")
@@ -31,9 +37,15 @@ object ServerMain extends App with ImplicitLazyLogging {
   debug"Akka is prepared"
   val config = Config.fromEnv
   val driverCP = "io.github.webuilt.sjdbc.MyDriver"
-  val dbUrl = config.get("C4_SPJR")
-    .getOrElse("jdbc:my:url=https://syncpost.dev.cone.ee/cto-tests-http")
-  val hostUrl: String = dbUrl.replaceFirst("jdbc:my:url=", "").split("/").init.mkString("/")
+  val dbUrlRaw = config
+    .get("C4SPJR")
+    .orElse(config.get("C4_SPJR"))
+    .getOrElse("jdbc:my:url=https://syncpost.dev.cone.ee&user=ase")
+  val dbUrlExtractR(hostUrlRaw, user) = dbUrlRaw
+  val hostUrl: String = if (hostUrlRaw.lastOption.contains('/'))
+                          hostUrlRaw.init
+                        else hostUrlRaw
+  val dbUrl = s"jdbc:my:url=$hostUrl/cto-tests-http&user=$user"
   debug"loaded DB configuration: [$dbUrl] with driver: $driverCP"
   lazy val driverInit: Boolean = Try(MyDriver.getClass).isSuccess
   info"driver$driverCP is ${
@@ -43,30 +55,35 @@ object ServerMain extends App with ImplicitLazyLogging {
   def conn(
     props: java.util.Properties = new java.util.Properties()
   ): java.sql.Connection = if (driverInit) DriverManager.getConnection(dbUrl, props)
-                           else throw new Exception("couldnt connect to db")
+                           else throw new Exception("couldn't connect to db")
   info"connected to $dbUrl"
-  val reportRegex = """([\w\d_-]*)\.pdf""".r
-  val reportRegexInner = """(.*)\.pdf\?(.*)""".r
-  val requestRoute: Route =
-    pathPrefix("request") {
-      path(".*".r) {
-        repName =>
+  val reportRoute: Route =
+    pathPrefix("report") {
+      path(pdfR) {
+        repName: String =>
           post {
             request: RequestContext =>
               request.complete {
-                info"request for report received"
+                info"request for report of type $repName received"
                 val response = {
-                  val paramsMap = new java.util.HashMap[String, Object]()
-                  val properties = new java.util.Properties()
-                  request.request.headers.toList.foreach {
-                    header =>
-                      paramsMap.put(header.name, header.value)
-                    //properties.put(k, v)
+                  val reportParameters: util.HashMap[String, Object] = new java.util.HashMap[String, Object]()
+                  val connectionProperties: Properties = new java.util.Properties()
+                  //todo adequate split
+                  request.request.headers
+                    .filterNot(_.name.equalsIgnoreCase("user"))
+                    .toList.foreach { header =>
+                    reportParameters.put(header.name, header.value)
                   }
-
-                  val jpr: JasperPrint = JasperFillManager
-                    .fillReport(JasperCompileManager.compileReport(s"./reports/$repName.jrxml"),
-                      paramsMap, conn(properties)
+                  request.request.headers
+                    .filter(_.name.equalsIgnoreCase("user"))
+                    .toList.foreach { header =>
+                    connectionProperties.put(header.name, header.value)
+                  }
+                  val jpr: JasperPrint =
+                    JasperFillManager.fillReport(
+                      JasperCompileManager.compileReport(s"./reports/$repName.jrxml"),
+                      reportParameters,
+                      conn(connectionProperties)
                     )
                   info"report compiled and filled"
                   val exporter: JRPdfExporter = new JRPdfExporter()
@@ -80,7 +97,7 @@ object ServerMain extends App with ImplicitLazyLogging {
                 Http(system).singleRequest(HttpRequest(
                   method = HttpMethods.POST,
                   uri = hostUrl + "/jasper-report",
-                  headers = request.request.headers.find(_.name.toLowerCase == "replyid").toList,
+                  headers = request.request.headers.find(_.name.equalsIgnoreCase("replyid")).toList,
                   entity = HttpEntity(response),
                 )
                 )
@@ -91,188 +108,12 @@ object ServerMain extends App with ImplicitLazyLogging {
           }
       }
     }
-  val reportRoute: Route =
-    pathPrefix("report") {
-      concat(
-        path(reportRegex) {
-          reportName =>
-
-            parameterMap {
-              pMap =>
-                get {
-                  request: RequestContext =>
-                    request.complete {
-                      info"request for report received"
-                      val response = {
-                        val paramsMap = new java.util.HashMap[String, Object]()
-                        val properties = new java.util.Properties()
-                        pMap.toList.foreach {
-                          case (k, v) =>
-                            paramsMap.put(k, v)
-                          //properties.put(k, v)
-                        }
-
-                        val jpr: JasperPrint = JasperFillManager
-                          .fillReport(JasperCompileManager.compileReport(s"./reports/$reportName.jrxml"),
-                            paramsMap, conn(properties)
-                          )
-                        info"report compiled and filled"
-                        val exporter: JRPdfExporter = new JRPdfExporter()
-                        exporter.setExporterInput(new SimpleExporterInput(jpr))
-                        val outs: ByteArrayOutputStream = new java.io.ByteArrayOutputStream()
-                        exporter.setExporterOutput(new SimpleOutputStreamExporterOutput(outs))
-                        exporter.exportReport()
-                        outs.toByteArray
-                      }
-                      info"sending completed report back"
-                      HttpResponse(status = StatusCodes.OK,
-                        entity = HttpEntity(response)
-                      )
-                    }
-                }
-            }
-        },
-        pathEndOrSingleSlash {
-          get {
-            complete {
-              val d = new File("./reports")
-              val fileList =
-                if (d.exists && d.isDirectory)
-                  d.listFiles.filter(_.isFile).toList
-                else
-                  List[File]()
-
-              HttpResponse(
-                status = StatusCodes.OK,
-                entity = HttpEntity(fileList.map(_.getName).mkString("\n").getBytes("UTF-8"))
-              )
-            }
-          }
-        }
-      )
-    }
-  val jrRoute: Route =
-    path("jr.pdf") {
-      get {
-        complete {
-          info"request for report received"
-          val jpr: JasperPrint = JasperFillManager
-            .fillReport(JasperCompileManager.compileReport("./vone.jrxml"),
-              new java.util.HashMap[String, Object](), conn()
-            )
-          info"report compiled and filled"
-          val exporter: JRPdfExporter = new JRPdfExporter()
-          exporter.setExporterInput(new SimpleExporterInput(jpr))
-          val outs: ByteArrayOutputStream = new java.io.ByteArrayOutputStream()
-          exporter.setExporterOutput(new SimpleOutputStreamExporterOutput(outs))
-          exporter.exportReport()
-          val bytes: Array[Byte] = outs.toByteArray
-          info"sending completed report back"
-          HttpResponse(entity = HttpEntity(bytes))
-        }
-      }
-    }
-  val storeReportRegex = """([\w\d_\-]*.jrxml)""".r
-  val storeReportRoute: Route =
-    pathPrefix("store") {
-      path(storeReportRegex) {
-        reportName =>
-          post {
-            req =>
-              info"new report $reportName storing..."
-              for {
-                body <- Unmarshal(req.request.entity).to[String]
-                compl <- req.complete {
-                  Using.resource(new PrintWriter(new File(s"./reports/$reportName"))) {
-                    _.write(body)
-                  }
-                  HttpResponse(status = StatusCodes.Created)
-                }
-                _ = info"new report stored"
-              } yield compl
-          }
-      }
-    }
-  val reportsListRoute: Route =
-    pathPrefix("list") {
-      concat(
-        pathEndOrSingleSlash {
-          post {
-            complete {
-              val d = new File("./reports")
-              val fileList =
-                if (d.exists && d.isDirectory)
-                  d.listFiles.filter(_.isFile).toList
-                else
-                  List[File]()
-
-              HttpResponse(
-                status = StatusCodes.OK,
-                entity = HttpEntity(fileList.map(_.getName).mkString("\n").getBytes("UTF-8"))
-              )
-            }
-          }
-        },
-        path(storeReportRegex) {
-          reportName =>
-            post {
-              complete {
-                val filePath = Paths.get(s"./reports/$reportName")
-                val exists = Files.exists(filePath)
-                lazy val dateCreated = Files.getLastModifiedTime(filePath)
-                val responseString =
-                  if (exists)
-                    s"report $reportName exists and created at ${dateCreated.toString}"
-                  else
-                    s"report $reportName does not exists on server"
-                HttpResponse(
-                  status = StatusCodes.OK,
-                  entity = HttpEntity(responseString.getBytes("UTF-8"))
-                )
-              }
-            }
-        },
-        path("""(.*)""".r) {
-          _ =>
-            post {
-              complete {
-                HttpResponse(
-                  status = StatusCodes.BadRequest
-                )
-              }
-            }
-        }
-      )
-    }
-  val downloatTemplateRoute: Route =
+  val templateRoute: Route =
     pathPrefix("templates") {
       concat(
-        path(storeReportRegex) {
-          reportName =>
-            post {
-              request: RequestContext =>
-                request.complete {
-                  info"request for template received for $reportName"
-                  val filePath = Paths.get(s"./reports/$reportName")
-                  val fileContent = Files.readAllBytes(filePath)
-                  info"file $reportName found"
-                  Http().singleRequest(HttpRequest(
-                    method = HttpMethods.POST,
-                    uri = hostUrl + "/jasper-template",
-                    headers = request.request.headers.find(_.name.toLowerCase == "replyid").toList,
-                    entity = HttpEntity(fileContent),
-                  )
-                  )
-                  info"successfully replied"
-                  HttpResponse(status = StatusCodes.OK,
-                    entity = HttpEntity(fileContent)
-                  )
-                }
-            }
-        },
         pathEndOrSingleSlash {
           post {
-            request =>
+            request: RequestContext =>
               info"templates list request received"
               val d = new File("./reports")
               val fileList =
@@ -299,12 +140,54 @@ object ServerMain extends App with ImplicitLazyLogging {
                 )
               )
           }
+        },
+        path(jrxmlR) {
+          reportName: String =>
+            post {
+              request: RequestContext =>
+                request.complete {
+                  info"request for template received for $reportName"
+                  val filePath = Paths.get(s"./reports/$reportName.jrxml")
+                  val fileContent = Files.readAllBytes(filePath)
+                  info"file $reportName found"
+                  Http().singleRequest(HttpRequest(
+                    method = HttpMethods.POST,
+                    uri = hostUrl + "/jasper-template",
+                    headers = request.request.headers.find(_.name.toLowerCase == "replyid").toList,
+                    entity = HttpEntity(fileContent),
+                  )
+                  )
+                  info"successfully replied"
+                  HttpResponse(status = StatusCodes.OK,
+                    entity = HttpEntity(fileContent)
+                  )
+                }
+            }
+        },
+        pathPrefix("upload") {
+          path(jrxmlR) {
+            reportName: String =>
+              post {
+                request: RequestContext =>
+                  info"new report $reportName storing..."
+                  for {
+                    body <- Unmarshal(request.request.entity).to[String]
+                    compl <- request.complete {
+                      Using.resource(new PrintWriter(new File(s"./reports/$reportName.jrxml"))) {
+                        _.write(body)
+                      }
+                      HttpResponse(status = StatusCodes.Created)
+                    }
+                    _ = info"new report stored"
+                  } yield compl
+              }
+          }
         }
       )
     }
   val (interface, port) = "0.0.0.0" -> 1080
   val bindingFuture: Future[Http.ServerBinding] = Http().bindAndHandle(
-    requestRoute ~ jrRoute ~ storeReportRoute ~ reportsListRoute ~ reportRoute ~ downloatTemplateRoute,
+    reportRoute ~ templateRoute,
     interface, port
   )
   info"successfully binded port $port\nwaiting to requests"
